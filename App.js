b55762786363629882697d7d93d9d5bbac59f05e
Abdul Hameed <raza2022@gmail.com>
2018-11-24 16:19:25 +0500

    project initialized - 1

--- App.js
+++ App.js
@@ -16,14 +16,19 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
+const messages = Platform.select({
+  ios: 'You are on an IOS Device',
+  android: 'You are on an Android Device',
+});
+
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.welcome}>Meteor React Demo!</Text>
+        <Text style={styles.instructions}>Test is always best</Text>
+        <Text style={styles.instructions}>{messages}</Text>
       </View>
     );
   }
